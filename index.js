8f44917b39558896f1e459a15adfe6210455bb68
afozbek <furkanozbek1995@gmail.com>
2019-08-25 20:11:12 +0300

    adding css classes dynamicly

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 new Vue({
     el: "#root",
     data: {
-        name: "Furkan"
-    },
-    computed: {},
-    watch: {
-        name: function(name) {
-            var vm = this;
-
-            setTimeout(function() {
-                vm.name = "Hello World 👩🏻";
-            }, 2000);
-        }
+        attachRed: false
     }
 });
